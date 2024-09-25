6d5ba7f99cdf4ea3f78163358aba63d43b8619eb
Gaetan <150467427+GaetanAL@users.noreply.github.com>
2024-09-25 17:22:55 -0400

    Partie 1

--- TP2.py
+++ TP2.py
@@ -1,21 +1,27 @@
 """
 TP2 : Système de gestion de livres pour une bibliothèque
 
-Groupe de laboratoire : XX
-Numéro d'équipe :  YY
-Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
+Groupe de laboratoire : 01  
+Numéro d'équipe : 01
+Noms et matricules : Gaetan Lohier (Matricule1), Nom2 (Matricule2)
 """
+import csv
 
 ########################################################################################################## 
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+csv_collection = open("collection_bibliotheque.csv", newline='')
 
+collection = csv.reader(csv_collection)
 
+bibliotheque  = dict()
 
-
-
+for c in collection:
+    bibliotheque[c[-1]] = c[:-1]
+    
+print(f' \n Bibliotheque initiale : {bibliotheque} \n')
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
